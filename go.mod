375680060b8157efa209df2aac5a626db77b2e02
David Coy <dcoy@gitlab.com>
2024-03-14 15:26:35 -0500

    Init commit
    Signed-off-by: David Coy <dcoy@gitlab.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/dcoy/simple-http-endpoint
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Request struct {
+	Properties struct {
+		ID            string                 `json:"id"`
+		AuthorID      int                    `json:"author_id"`
+		AuthorName    string                 `json:"author_name"`
+		Details       map[string]interface{} `json:"details"`
+		IPAddress     string                 `json:"ip_address"`
+		EntityID      int                    `json:"entity_id"`
+		EntityPath    string                 `json:"entity_path"`
+		EntityType    string                 `json:"entity_type"`
+		EventType     string                 `json:"event_type"`
+		TargetID      int                    `json:"target_id"`
+		TargetType    string                 `json:"target_type"`
+		TargetDetails string                 `json:"target_details"`
+	} `json:"properties"`
+	Type string `json:"type"`
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	var req Request
+	if err := json.Unmarshal(body, &req); err != nil {
+		http.Error(w, "Error parsing JSON request body", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received Request: %+v\n", req)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Request processed successfully"))
+}
+
+func main() {
+	http.HandleFunc("/post", postHandler)
+	fmt.Println("Server is listening on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
